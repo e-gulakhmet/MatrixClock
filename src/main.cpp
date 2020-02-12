0d2fa9a568f222041e0f6353a82187d53ebc9d47
Enes <enes515254@gmail.com>
2020-02-13 01:21:56 +0600

    Add ds3231 library

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,14 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Max72xxPanel.h>
+#include <RTClib.h>
 
 #include "main.h"
 
 Max72xxPanel matrix = Max72xxPanel(9, 1, 4);
+RTC_DS3231 rtc;
 
-
-
-
-
-
-
-
-
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void drawNum(uint8_t num, uint8_t x, uint8_t y) {
   for (int r = 0; r < 8; r++) {
@@ -27,6 +22,12 @@ void drawNum(uint8_t num, uint8_t x, uint8_t y) {
 
 
 void setup() {
+  Serial.begin(9600);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   matrix.setIntensity(15);                    // Задаем яркость от 0 до 15
   matrix.setRotation(3);
   matrix.fillScreen(LOW);                       // Обнуление матрицы
@@ -35,13 +36,34 @@ void setup() {
 
 
 void loop() {
-  // for (int y = 0; y < 8; y++ ) {
-  //   for (int x = 0; x < 32; x++ ) { // Передача массива
-  //     matrix.drawPixel(x, y, HIGH);
-  //     matrix.write();
-  //     delay(200);
-  //   }
-  // }
-  drawNum(9, 0, 0);
   matrix.write();
+
+  DateTime now = rtc.now();
+
+  drawNum(now.hour() / 10, 0, 0);
+  drawNum(now.hour() % 10, 8, 0);
+  drawNum(now.minute() / 10, 16, 0);
+  drawNum(now.minute() % 10, 22, 0);
+
+
+  Serial.print(now.year());
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  Serial.print("Temperature: ");
+  Serial.print(rtc.getTemperature());
+  Serial.println(" C");
+
+  delay(3000);
 }
