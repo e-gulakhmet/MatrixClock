5c7f2866d648350b80befdfc997989038d542649
Enes <enes515254@gmail.com>
2020-02-12 22:55:39 +0600

    Add draw number function

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,45 @@
 #include <Adafruit_GFX.h>
 #include <Max72xxPanel.h>
 
+#include "main.h"
+
 Max72xxPanel matrix = Max72xxPanel(9, 1, 4);
+
+
+
+
+
+
+
+
+
+
+void drawNum(uint8_t num, uint8_t x, uint8_t y) {
+  for (int r = 0; r < 8; r++) {
+    for (int c = 0; c < 6; c++) {
+      matrix.drawPixel(x + c, y + r, numbers[num][r] & (1 << c));
+    }
+  }  
+}
+
+
 
 void setup() {
   matrix.setIntensity(15);                    // Задаем яркость от 0 до 15
   matrix.setRotation(3);
+  matrix.fillScreen(LOW);                       // Обнуление матрицы
 }
 
+
+
 void loop() {
-  matrix.fillScreen(LOW);                       // Обнуление матрицы
-  for (int y = 0; y < 8; y++ ) {
-    for (int x = 0; x < 32; x++ ) { // Передача массива
-      matrix.drawPixel(x, y, HIGH);
-      matrix.write();
-      delay(200);
-    }
-  }
+  // for (int y = 0; y < 8; y++ ) {
+  //   for (int x = 0; x < 32; x++ ) { // Передача массива
+  //     matrix.drawPixel(x, y, HIGH);
+  //     matrix.write();
+  //     delay(200);
+  //   }
+  // }
+  drawNum(9, 0, 0);
+  matrix.write();
 }
