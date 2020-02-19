ad1de93f7ac4a2df3a6a7ea0ef7e03db6e79310d
Enes <enes515254@gmail.com>
2020-02-20 01:12:02 +0600

    Add dht sensor

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
 #include <Wire.h>
 #include <RTClib.h>
 #include <OneButton.h>
+#include <DHT.h>
 
 #include "main.h"
 #include "digit.h"
 #include "power.h"
+#include "object.h"
 
 
 // TODO: Добавить датчик температуры
@@ -19,9 +21,10 @@ Max72xxPanel matrix = Max72xxPanel(MATRIX_CS_PIN, 1, 4);
 RTC_DS3231 rtc;
 OneButton button1(BUTT_1_PIN, false);
 OneButton button2(BUTT_2_PIN, false);
+DHT dht(2, DHT11); 
 Power battery(POWER_SENSOR_PIN);
 
-MainMode main_mode = mmPower;
+MainMode main_mode = mmClock;
 
 bool show_dots;
 unsigned long timer;
@@ -74,18 +77,38 @@ void drawNum(uint8_t num, uint8_t num_len, const byte numbers[10][8], uint8_t x,
 
 
 
+void drawChar(const byte object[8], uint8_t w, uint8_t h, uint8_t x, uint8_t y) { // Функция вывода объектов
+  // Первый параметр - объект, который хотим вывести.
+  // Второй параметр - ширина объекта
+  // Третий параметр - длина объекта
+  // Четвертый и пятый парамерт - расположение
+  for (int r = 0; r <= h; r++) {
+    for (int c = 0; c <= w; c++) {
+      matrix.drawPixel(x + c, y + r, object[r] & (1 << c));
+    }
+  }  
+
+}
+
+
+
 void showDisp() {
   switch (main_mode) {
     case mmClock: { // Выводим время на дисплей
-      DateTime now = rtc.now();
-      drawNum(now.hour() / 10, 5, numbers, 0, 0);
-      drawNum(now.hour() % 10, 5, numbers, 6, 0);
-      drawNum(now.minute() / 10, 5, numbers, 19, 0);
-      drawNum(now.minute() % 10, 5, numbers, 26, 0);
-      // Выводим точки
-      show_dots = !show_dots;
-      matrix.drawRect(15, 1, 2, 2, show_dots);
-      matrix.drawRect(15, 5, 2, 2, show_dots);
+      if (rtc.begin()) { // Если модуль времени подключен
+        DateTime now = rtc.now();
+        drawNum(now.hour() / 10, 5, numbers, 0, 0);
+        drawNum(now.hour() % 10, 5, numbers, 7, 0);
+        drawNum(now.minute() / 10, 5, numbers, 19, 0);
+        drawNum(now.minute() % 10, 5, numbers, 26, 0);
+        // Выводим точки
+        show_dots = !show_dots;
+        matrix.drawRect(15, 1, 2, 2, show_dots);
+        matrix.drawRect(15, 5, 2, 2, show_dots);
+      }
+      else { // Если модуль времени не подключен
+        matrix.print("ERROR");
+      }
     }break;
     
     case mmDate: { // Выводим дату на матрицу
@@ -102,6 +125,7 @@ void showDisp() {
     }break;
 
     case mmTemp: {
+      matrix.print(dht.readTemperature());
     }break;
 
     case mmPower: { // Выводим данные о батареи
@@ -110,8 +134,9 @@ void showDisp() {
         drawNum(0, 4, small_numbers, 5, 0);
         drawNum(0, 4, small_numbers, 10, 0);
       }
-        drawNum(battery.getProcent() / 10, 4, small_numbers, 5, 0);
-        drawNum(battery.getProcent() % 10, 4, small_numbers, 10, 0);
+        drawNum(battery.getProcent() / 10, 4, small_numbers, 0, 0);
+        drawNum(battery.getProcent() % 10, 4, small_numbers, 5, 0);
+        drawChar(procent, 6, 8, 10, 0);
     }break;
   }
 
@@ -133,6 +158,8 @@ void setup() {
   }
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Устанавливаем время, которое указанно на компьютере
 
+  dht.begin();
+
   matrix.setIntensity(10); // Задаем яркость от 0 до 15
   matrix.setRotation(3);
   matrix.fillScreen(LOW); // Обнуление матрицу
@@ -153,5 +180,5 @@ void loop() {
     timer = millis();
     showDisp();
   }
-
+  Serial.println(dht.readTemperature());
 }
---
+++ src/object.h
@@ -0,0 +1,19 @@
+#ifndef _OBJECT_H_
+#define _OBJECT_H_
+
+#include <Arduino.h>
+
+
+const byte procent[8] = {
+    B100111,
+    B100101,
+    B010111,
+    B001000,
+    B000100,
+    B111010,
+    B101001,
+    B111001
+};
+
+
+#endif // _OBJECT_H_
