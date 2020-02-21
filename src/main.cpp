f48c14528d24df2143e0cd15be12f93037ebc2bf
Enes <enes515254@gmail.com>
2020-02-21 22:17:56 +0600

    Add Weather information

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,6 @@
 #include "power.h"
 #include "object.h"
 
-
-// TODO: Добавить датчик температуры
 // TODO: Сделать эффект перехода между страницами
 
 
@@ -29,6 +27,7 @@ MainMode main_mode = mmClock;
 bool show_dots;
 unsigned long timer;
 unsigned long batt_timer;
+bool second_object;
 
 
 
@@ -68,8 +67,8 @@ void drawNum(uint8_t num, uint8_t num_len, const byte numbers[10][8], uint8_t x,
   // Второй параметр - цифра, которая указывает длину цифры(смотреть в digit.h)
   // Третий параметр - массив в котором содержатся цифры
   // Четвертый и пятый парамерт - расположение цифр
-  for (int r = 0; r <= 7; r++) {
-    for (int c = 0; c <= num_len; c++) {
+  for (int r = 0; r < 8; r++) {
+    for (int c = 0; c < num_len; c++) {
       matrix.drawPixel(x + c, y + r, numbers[num][r] & (1 << c));
     }
   }  
@@ -77,13 +76,13 @@ void drawNum(uint8_t num, uint8_t num_len, const byte numbers[10][8], uint8_t x,
 
 
 
-void drawChar(const byte object[8], uint8_t w, uint8_t h, uint8_t x, uint8_t y) { // Функция вывода объектов
+void drawObject(const byte object[8], uint8_t w, uint8_t h, uint8_t x, uint8_t y) { // Функция вывода объектов
   // Первый параметр - объект, который хотим вывести.
   // Второй параметр - ширина объекта
   // Третий параметр - длина объекта
   // Четвертый и пятый парамерт - расположение
-  for (int r = 0; r <= h; r++) {
-    for (int c = 0; c <= w; c++) {
+  for (int r = 0; r < h; r++) {
+    for (int c = 0; c < w; c++) {
       matrix.drawPixel(x + c, y + r, object[r] & (1 << c));
     }
   }  
@@ -97,10 +96,10 @@ void showDisp() {
     case mmClock: { // Выводим время на дисплей
       if (rtc.begin()) { // Если модуль времени подключен
         DateTime now = rtc.now();
-        drawNum(now.hour() / 10, 5, numbers, 0, 0);
-        drawNum(now.hour() % 10, 5, numbers, 7, 0);
-        drawNum(now.minute() / 10, 5, numbers, 19, 0);
-        drawNum(now.minute() % 10, 5, numbers, 26, 0);
+        drawNum(now.hour() / 10, 6, numbers, 0, 0);
+        drawNum(now.hour() % 10, 6, numbers, 7, 0);
+        drawNum(now.minute() / 10, 6, numbers, 19, 0);
+        drawNum(now.minute() % 10, 6, numbers, 26, 0);
         // Выводим точки
         show_dots = !show_dots;
         matrix.drawRect(15, 1, 2, 2, show_dots);
@@ -124,19 +123,32 @@ void showDisp() {
       drawNum((now.year() - 2000) % 10, 4, small_numbers, 27, 0);
     }break;
 
-    case mmTemp: {
-      matrix.print(dht.readTemperature());
+    case mmTemp: { // Выводим данные о погоде на матрицу
+      // Показываем температуру
+      drawNum(int(dht.readTemperature()) / 10, 4, small_numbers, 0, 0);
+      drawNum(int(dht.readTemperature()) % 10, 4, small_numbers, 5, 0);
+      drawObject(gradus, 3, 3, 10, 0);
+      drawObject(C, 4, 8, 14, 0);
+      second_object = !second_object;
+      // Показываем погоду по температуре
+      if (int(dht.readTemperature()) / 10 > 25) {
+        drawObject(sun[second_object], 7, 7, 21, 1);
+      }
+      else {
+        drawObject(cloud[second_object], 8, 8, 21, 0);
+      }
+
     }break;
 
     case mmPower: { // Выводим данные о батареи
       if (battery.getProcent() == 100) {
-        drawNum(battery.getProcent() / 100, 4, small_numbers, 0, 0);
-        drawNum(0, 4, small_numbers, 5, 0);
-        drawNum(0, 4, small_numbers, 10, 0);
+        drawNum(battery.getProcent() / 100, 4, small_numbers, 8, 0);
+        drawNum(0, 4, small_numbers, 13, 0);
+        drawNum(0, 4, small_numbers, 18, 0);
       }
-        drawNum(battery.getProcent() / 10, 4, small_numbers, 0, 0);
-        drawNum(battery.getProcent() % 10, 4, small_numbers, 5, 0);
-        drawChar(procent, 6, 8, 10, 0);
+        drawNum(battery.getProcent() / 10, 4, small_numbers, 8, 0);
+        drawNum(battery.getProcent() % 10, 4, small_numbers, 13, 0);
+        drawObject(procent, 6, 8, 18, 0);
     }break;
   }
 
@@ -180,5 +192,4 @@ void loop() {
     timer = millis();
     showDisp();
   }
-  Serial.println(dht.readTemperature());
 }
